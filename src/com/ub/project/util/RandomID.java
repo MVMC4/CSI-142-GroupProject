ddaffa6414ba193cdb50d1334db075887db43f18
Pumbernator <maatla002@gmail.com>
2026-04-27 01:50:20 +0200

    Updated RandomID.java

---
+++ src/com/ub/project/util/RandomID.java
@@ -0,0 +1,20 @@
+function generateId(length = 10) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.random() * 16 | 0;
+    const v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+// Usage
+console.log(generateId(8));  // e.g. "K7nP4mX9"
+console.log(generateUUID()); // e.g. "123e4567-e89b-12d3-a456-426614174000"
